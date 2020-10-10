ea48f2623acebbbc543ceeb6a7301b8090a5001c
root <root@devx-jenkins001.ziyotek.local>
2020-10-10 12:12:44 -0400

    pipelne code

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def add(x, y):
+	return x + y
+
+def subtract(x, y):
+	return x - y
+
+add_answer = add(2, 2)
+print("Add answer for add(2, 2) is "+ str(add_answer))
+subtract_answer = subtract(4, 2)
+print("Subtract answer for subtract(4, 2) is "+ str(subtract_answer))
